245a048490c86ed0ac3fe82ec47ef408cfa33288
Prashant Kumar Upadhyay <pkumarupadhyay0@gmail.com>
2023-10-31 11:49:35 +0530

    n

--- src/ElgamalCipher.java
+++ src/ElgamalCipher.java
@@ -1,18 +1,54 @@
+import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.Scanner;
 
-public class ElgamalCipher {
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n =sc.nextInt();
+public class ElgamalEncryption {
+    private static BigInteger p; // Prime number
+    private static BigInteger g; // Primitive root modulo p
+    private static BigInteger x; // Private key
+    private static BigInteger y; // Public key
+    private static BigInteger k; // Random value
 
-        switch(n){
-            case 1: 
-            String plaintext = sc.next();
-            encrypt(plaintext);
-            break;
-            case 2: 
-            break;
+    public static void main(String[] args) {
+        // Generate keys
+        generateKeys() ;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Choose operation (1 for Encryption, 2 for Decryption): ");
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            // Input plaintext to be encrypted
+            System.out.print("Enter plaintext to encrypt: ");
+            scanner.nextLine(); // Consume the newline character
+            String plaintext = scanner.nextLine();
+
+            // Encrypt the plaintext
+            BigInteger[] ciphertext = encrypt(plaintext);
+
+            System.out.println("Ciphertext: (" + ciphertext[0] + ", " + ciphertext[1] + ")");
+        } else if (choice == 2) {
+            // Input ciphertext to be decrypted
+            System.out.print("Enter ciphertext to decrypt (a b): ");
+            BigInteger a = scanner.nextBigInteger();
+            BigInteger b = scanner.nextBigInteger();
+
+            BigInteger[] ciphertext = {a, b};
+
+            // Decrypt the ciphertext
+            String decryptedText = decrypt(ciphertext);
+
+            System.out.println("Decrypted Text: " + decryptedText);
+        } else {
+            System.out.println("Invalid choice. Please choose 1 for Encryption or 2 for Decryption.");
         }
+
+        scanner.close();
     }
+
+    private static BigInteger[] encrypt(String plaintext) {
+        return null;
+    }
+
+    // Rest of the code remains the same as in the previous example...
 }
